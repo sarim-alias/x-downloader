d7a19ba8ae95497b7e87ed5f3f2b30bac12159bf
sarim-alias <sarimsitenative@gmail.com>
2025-03-12 14:58:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def download():
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5004)
